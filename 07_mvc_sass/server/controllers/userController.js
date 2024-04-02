7f4a8462ea5d461eeb6ed669f8720d166a0152c8
FERNANDA ALVES CORDEIRO CONTE SILVA <fernanda.accsilva@senacsp.edu.br>
2024-04-01 22:11:22 -0300

    aula 01-04-2024

--- 07_mvc_sass/server/controllers/userController.js
+++ 07_mvc_sass/server/controllers/userController.js
@@ -1,25 +1,102 @@
-const mysql = require('mysql2')
+const mysql = require('mysql')
+
 
 let connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'usermanagement_tut'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'usermanagement_tut'
 })
 
-exports.view = (req, res) =>{
+exports.view = (req, res) => {
 
     //User the connection
-connection.query(`SELECT * FROM user WHERE status = "active"`, (err,rows) =>{
-   
-    //When done with the connection, release it
-    if(!err){
-        let removedUser = req.query.removed
-        res.render('home', {rows, removedUser})
-    } else{
-        console.log(err)
-    }
-    console.log('The data form user table: \n', rows)
+    connection.query(`SELECT * FROM user WHERE status = "active"`, (err, rows) => {
+
+        //When done with the connection, release it
+        if (!err) {
+            let removedUser = req.query.removed
+            res.render('home', { rows, removedUser })
+        } else {
+            console.log(err)
+        }
+        console.log('The data form user table: \n', rows)
+    })
 }
-)
+
+exports.find = (req, res) => {
+    let searchTerm = req.body.search;
+
+    connection.query(`SELECT * FROM  user WHERE first_name LIKE ? or last_name LIKE ?`, ['%' + searchTerm + '%', '%' + searchTerm + '%'], (err, rows) => {
+        if (!err) {
+            res.render('home', { rows })
+        } else {
+            console.log(err)
+        }
+        console.log('The data form user table: \n', rows)
+    })
 }
+
+exports.form = (req, res) => {
+    res.render('add-user')
+}
+
+exports.create = (req, res) => {
+    const { first_name, last_name, email, phone, comments } = req.body
+
+    let searchTerm = req.body.search
+
+    connection.query('INSERT INTO user SET  first_name = ?, last_name = ?, email = ?, phone =?, comments = ?', [first_name, last_name, email, phone, comments], (err, rows) => {
+        if (!err) {
+            res.render('add-user', { alert: 'User added sucessfully' })
+        } else {
+            console.log(err)
+        }
+        console.log('The data form user table: \n', rows)
+    })
+}
+
+exports.edit = (req, res) => {
+    connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err, rows) => {
+      if (!err) {
+        res.render('edit-user', { rows });
+      } else {
+        console.log(err);
+      }
+      console.log('The data from user table: \n', rows);
+    });
+  }
+  
+
+  //Update User
+  exports.update = (req, res) => {
+    const { first_name, last_name, email, phone, comments } = req.body;
+    connection.query('UPDATE user SET first_name = ?, last_name = ?, email = ?, phone = ?, comments = ? WHERE id = ?', [first_name, last_name, email, phone, comments, req.params.id], (err, rows) => {
+      if (!err) {
+        connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err, rows) => {      
+          if (!err) {
+            res.render('edit-user', { rows, alert: `${first_name} has been updated.` });
+          } else {
+            console.log(err);
+          }
+          console.log('The data from user table: \n', rows);
+        });
+      } else {
+        console.log(err);
+      }
+      console.log('The data from user table: \n', rows);
+    });
+  }
+
+  exports.delete = (req,res)=>{
+    connection.query('UPDATE user SET status = ? WHERE id = ?', ['removed', req.params.id], (err, rows) =>{
+        if (!err){
+            let removedUser = encodeURIComponent('User successfully removed')
+            res.redirect('/?removed=' + removedUser)
+        }else{
+            console.log(err)
+        }
+        console.log('The data from user table: \n', rows);
+    })
+
+  }
